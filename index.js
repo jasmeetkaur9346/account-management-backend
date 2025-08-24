950f192398f2ec19a713eef8e28354558fc36730
jasmeetkaur9346 <vacomputers.com@gmail.com>
2025-08-24 21:43:15 +0530

    index.js file update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 const connectDB = require("./config/db")
 
 // Import Routes
-const router = require('./routes/index')
+const routes = require('./routes/index')
 
 connectDB();
 
@@ -32,15 +32,14 @@ const allowedOrigins = [
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization"]
   }));
-
-  app.options('*', cors());
+  
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // API Routes
-app.use('/api', router);
+app.use('/api', routes);
 // Health Check Route
 app.get('/', (req, res) => {
   res.json({
